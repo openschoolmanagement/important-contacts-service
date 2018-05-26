352b3f91a7b224d32e9cb4a8a175a88ab193f55e
Philipp <philippkostyra@gmail.com>
2018-05-26 18:28:05 +0200

    Intial Spring Boot Setup
    Copy of Initializr Contents

---
+++ src/main/java/open/schoolmanagement/contacts/importantcontactsservice/ImportantContactsServiceApplication.java
@@ -0,0 +1,12 @@
+package open.schoolmanagement.contacts.importantcontactsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportantContactsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportantContactsServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/open/schoolmanagement/contacts/importantcontactsservice/ImportantContactsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package open.schoolmanagement.contacts.importantcontactsservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImportantContactsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
