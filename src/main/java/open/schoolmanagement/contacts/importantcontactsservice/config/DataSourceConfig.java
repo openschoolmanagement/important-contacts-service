64e70327400988a9ed44b6f0c140e1e395c6ec2f
Thomas Bonk <thomas@meandmymac.de>
2018-05-31 18:14:23 +0200

    fixed checksytle issue

--- src/main/java/open/schoolmanagement/contacts/importantcontactsservice/config/DataSourceConfig.java
+++ src/main/java/open/schoolmanagement/contacts/importantcontactsservice/config/DataSourceConfig.java
@@ -23,7 +23,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Configuration of the data source
+ * Configuration of the data source.
  */
 @Configuration
 public class DataSourceConfig {
