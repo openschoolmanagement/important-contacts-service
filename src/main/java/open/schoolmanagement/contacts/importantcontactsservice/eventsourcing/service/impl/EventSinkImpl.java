d3a731294c03b7ad13816780b06292cc3b8bc86c
Thomas Bonk <thomas@meandmymac.de>
2018-09-14 12:23:12 +0200

    - persisting events to EventStore

--- src/main/java/open/schoolmanagement/contacts/importantcontactsservice/eventsourcing/service/impl/EventSinkImpl.java
+++ src/main/java/open/schoolmanagement/contacts/importantcontactsservice/eventsourcing/service/impl/EventSinkImpl.java
@@ -16,30 +16,77 @@
 
 package open.schoolmanagement.contacts.importantcontactsservice.eventsourcing.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import open.schoolmanagement.contacts.importantcontactsservice.database.Transaction;
+import open.schoolmanagement.contacts.importantcontactsservice.database.TransactionManager;
 import open.schoolmanagement.contacts.importantcontactsservice.eventsourcing.events.Event;
+import open.schoolmanagement.contacts.importantcontactsservice.eventsourcing.model.EventProcessingResult;
+import open.schoolmanagement.contacts.importantcontactsservice.eventsourcing.model.StreamedEvent;
 import open.schoolmanagement.contacts.importantcontactsservice.eventsourcing.repository.StreamedEventRepository;
 import open.schoolmanagement.contacts.importantcontactsservice.eventsourcing.service.EventProcessingResultService;
 import open.schoolmanagement.contacts.importantcontactsservice.eventsourcing.service.EventSink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 class EventSinkImpl implements ApplicationListener<EventSourcingSpringEvent>, EventSink {
+  private final static Logger LOG = LoggerFactory.getLogger(EventSinkImpl.class);
+
   private final EventProcessingResultService eventProcessingResultService;
   private final StreamedEventRepository streamedEventRepository;
+  private final TransactionManager transactionManager;
 
   @Autowired
   EventSinkImpl(EventProcessingResultService eventProcessingResultService,
-      StreamedEventRepository streamedEventRepository) {
+      StreamedEventRepository streamedEventRepository,
+      TransactionManager transactionManager) {
 
     this.eventProcessingResultService = eventProcessingResultService;
     this.streamedEventRepository = streamedEventRepository;
+    this.transactionManager = transactionManager;
   }
 
   @Override
   public void accept(Event event) {
-    // TODO save event and process aggregate for read layer
+    try {
+      storeEvent(event);
+      updateReadModel(event);
+    } catch (JsonProcessingException ex) {
+      LOG.error("Error while persisting event", ex);
+
+      eventProcessingResultService
+          .addEventProcessingResult(
+              event.getEventId(),
+              EventProcessingResult
+                  .builder()
+                  .eventId(event.getEventId())
+                  .successful(false)
+                  .message("Error while persisting event. This results in dataloss!")
+                  .build());
+    }
+  }
+
+  private void storeEvent(Event event) throws JsonProcessingException {
+    Transaction ta = transactionManager.begin();
+
+    streamedEventRepository
+        .save(
+            StreamedEvent
+                .from(
+                    event));
+
+    transactionManager.commit(ta);
+  }
+
+  private void updateReadModel(Event event) {
+    Transaction ta = transactionManager.begin();
+
+    // TODO
+
+    transactionManager.commit(ta);
   }
 
   @Override
